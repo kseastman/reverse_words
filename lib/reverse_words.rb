c9d4b172b4fc1b4cc0eb1242921dcf4dbc5ae808
Kat <kat.eastman@gmail.com>
2018-03-25 12:53:49 -0500

    new solution

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,38 +1,43 @@
 require 'pry'
 # A method to reverse each word in a sentence, in place.
+
+
 def reverse_words(my_words)
-  return my_words if my_words == nil || my_words.length == 0
+  return nil if my_words == nil
+  return my_words if my_words.length == 1
 
-  index_array = []
-  index = -1
+  index = 0
 
-  while index = my_words.index(" ", index + 1)
-    index_array << index
-  end
-
-  if index_array.length == 0
-    length = 1
-  else
-    length = index_array.length
-  end
-
-  start_index = 0
-
-  length.times do |i|
-    unless start_index == 0
-      start_index = index_array[i - 1]
+  while index < my_words.length
+    while my_words[index] == " "
+      index += 1
     end
-    end_index = index_array[i] - 1
 
-    while start_index < end_index
-      temp_storage = my_words[start_index]
-      my_words[start_index] = my_words[end_index]
-      my_words[end_index] = temp_storage
-      start_index += 1
+    inner_index = index
+
+    while my_words[inner_index] != " " && inner_index < my_words.length - 1
+      inner_index += 1
+    end
+
+    begin_index = index
+  end_index = inner_index
+
+    while begin_index < end_index
+      while my_words[begin_index] == " "
+        begin_index += 1
+      end
+
+      while my_words[end_index] == " "
       end_index -= 1
-    end
+      end
 
-    i += 1
+      temp = my_words[begin_index]
+      my_words[begin_index] = my_words[end_index]
+      my_words[end_index] = temp
+      begin_index += 1
+    end_index -= 1
+    end
+    index = inner_index + 1
   end
   return my_words
 end
